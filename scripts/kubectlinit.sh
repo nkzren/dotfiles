2e1810d85078985188e57094d42b84eebd345cd1
Renan Nakazawa <renan.nakazawa0@gmail.com>
2021-06-26 03:54:18 -0300

    add scripts

---
+++ scripts/kubectlinit.sh
@@ -0,0 +1,6 @@
+# Kubernetes related aliases
+
+alias k=kubectl 
+alias kcd='kubectl config set-context $(kubectl config current-context) --namespace '
+alias kccd='kubectl config use-context '
+alias kcgc='kubectl config get-contexts'
