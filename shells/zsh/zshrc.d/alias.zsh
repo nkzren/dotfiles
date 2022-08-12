b71538c2d0a0fe0c3a9d9da486fd3d5e4e841577
nkzren <renan.nakazawa0@gmail.com>
2022-08-12 00:57:39 -0300

    move aliases

--- shells/zsh/zshrc.d/alias.zsh
+++ shells/zsh/zshrc.d/alias.zsh
@@ -1,5 +1,38 @@
 alias dotfiles="~/Documents/dev/dotfiles"
 alias zshrc="vi ~/.zshrc"
 
+# Docker
+if [ ! -z $(command -v docker) ]; then
+    alias d=docker
+    alias dc=docker-compose
+fi
 
-RPROMPT='$(kube_ps1)'
+# Kubernetes related aliases
+if [ ! -z $(command -v kubectl) ]; then
+    alias k=kubectl 
+    alias kcd='kubectl config set-context $(kubectl config current-context) --namespace '
+    alias kccd='kubectl config use-context '
+    alias kcgc='kubectl config get-contexts'
+
+    if [ ! -z $(command -v nvim) ]; then
+        export KUBE_EDITOR='nvim'
+    fi
+fi
+
+# TMUX
+if [ ! -z $(command -v tmux) ]; then
+    alias t='tmux attach || tmux new-session'
+
+    # Attaches tmux to a session (example: ta portal)
+    alias ta='tmux attach -t'
+
+    # Creates a new session
+    alias tn='tmux new-session'
+
+    # Lists all ongoing sessions
+    alias tl='tmux list-sessions'
+fi
+
+if [ ! -z $(command -v helm) ]; then
+    source $UTIL_SCRIPTS_DIR/helm-completion.sh
+fi
--- shells/zsh/zshrc.d/docker.zsh
+++
@@ -1,4 +0,0 @@
-DOCKER_INIT_SCRIPT=$UTIL_SCRIPTS_DIR/dockerinit.sh
-if [ ! -z $(command -v docker) ] && [ -f "$DOCKER_INIT_SCRIPT" ]; then
-    source $DOCKER_INIT_SCRIPT
-fi
