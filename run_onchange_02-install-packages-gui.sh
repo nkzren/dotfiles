c4f5b76231616e4126060269801af9e5a5c562b1
nkzren <renan.nakazawa0@gmail.com>
2022-09-27 15:11:24 -0300

    i3lock-color -> i3lock

--- run_onchange_02-install-packages-gui.sh
+++ run_onchange_02-install-packages-gui.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-yay -S --noconfirm i3-gaps i3blocks i3status i3lock-color \
+yay -S --noconfirm i3-gaps i3blocks i3status i3lock \
     rofi picom network-manager-applet lxappearance \
     pavucontrol spotify firefox vlc
 
