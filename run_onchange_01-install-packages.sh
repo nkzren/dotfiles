0bc1b7bae28925d9eb2ef8ff5766802f5ea8d259
nkzren <renan.nakazawa0@gmail.com>
2022-10-03 10:31:38 -0300

    update with debian-based

--- run_onchange_01-install-packages.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-yay -S --noconfirm tldr man-db vim neovim bat kitty ranger tmux byobu \
-    xclip feh jq curl net-tools python pipewire pipewire-pulse \
-    noto-fonts-cjk noto-fonts-emoji ntfs-3g ntfsfixboot sysstat ncdu \
-    nerd-fonts-hack nerd-fonts-mononoki ttf-nerd-fonts-symbols-2048-em-mono
-    
