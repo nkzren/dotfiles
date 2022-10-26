869217b8dcb39d4c7c8a741f1339a7e25f1057fc
nkzren <renan.nakazawa0@gmail.com>
2022-10-26 09:27:42 -0300

    clean zshrc.d

--- dot_zshrc.d/alias.zsh
+++ dot_zshrc.d/alias.zsh
@@ -1,36 +1,29 @@
-alias dotfiles="~/Documents/dev/dotfiles"
 alias zshrc="vi ~/.zshrc"
 
 # Docker
-if [ ! -z $(command -v docker) ]; then
-    alias d=docker
-    alias dc=docker-compose
-fi
+alias d=docker
+alias dc=docker-compose
 
 # Kubernetes related aliases
-if [ ! -z $(command -v kubectl) ]; then
-    alias k=kubectl 
-    alias kcd='kubectl config set-context $(kubectl config current-context) --namespace '
-    alias kccd='kubectl config use-context '
-    alias kcgc='kubectl config get-contexts'
+alias k=kubectl 
+alias kcd='kubectl config set-context $(kubectl config current-context) --namespace '
+alias kccd='kubectl config use-context '
+alias kcgc='kubectl config get-contexts'
 
-    if [ ! -z $(command -v nvim) ]; then
-        export KUBE_EDITOR='nvim'
-    fi
+if [ ! -z $(command -v nvim) ] && [ ! -z $(command -v kubectl) ]; then
+    export KUBE_EDITOR='nvim'
 fi
 
 # TMUX
-if [ ! -z $(command -v tmux) ]; then
-    alias t='tmux attach || tmux new-session'
+alias t='tmux attach || tmux new-session'
 
-    # Attaches tmux to a session (example: ta portal)
-    alias ta='tmux attach -t'
+# Attaches tmux to a session (example: ta portal)
+alias ta='tmux attach -t'
 
-    # Creates a new session
-    alias tn='tmux new-session'
+# Creates a new session
+alias tn='tmux new-session'
 
-    # Lists all ongoing sessions
-    alias tl='tmux list-sessions'
-fi
+# Lists all ongoing sessions
+alias tl='tmux list-sessions'
 
 alias c=chezmoi
