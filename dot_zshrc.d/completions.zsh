770db88296cc466d8b480bc6bdbafe0408805193
nkzren <renan.nakazawa0@gmail.com>
2023-01-13 08:10:29 -0300

    fix discord package

--- dot_zshrc.d/completions.zsh
+++ dot_zshrc.d/completions.zsh
@@ -1,5 +1,6 @@
 # Kube
 complete -F __start_kubectl k
 [[ $commands[kubectl] ]] && source <(kubectl completion zsh)
+[[ $commands[chezmoi] ]] && source <(chezmoi completion zsh)
 [[ $commands[helm] ]] && source <(helm completion zsh)
 [[ $commands[bw] ]] && eval "$(bw completion --shell zsh); compdef _bw bw;"
