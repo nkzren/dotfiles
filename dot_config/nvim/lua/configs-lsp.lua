dffbf19c370865971e208567966a81549232c25f
nkzren <renan.nakazawa0@gmail.com>
2023-03-10 15:32:37 -0300

    wip: migrar nvim configs

--- dot_config/nvim/lua/configs-lsp.lua
+++ dot_config/nvim/lua/configs-lsp.lua
@@ -2,6 +2,7 @@ local capabilities = require("cmp_nvim_lsp").default_capabilities()
 
 local lspconfig = require('lspconfig')
 
+
 -- Use an on_attach function to only map the following keys
 -- after the language server attaches to the current buffer
 local on_attach = function(client, bufnr)
@@ -31,18 +32,55 @@ local on_attach = function(client, bufnr)
 
   -- vim.keymap.set('n', 'K', vim.lsp.buf.hover, bufopts)
   -- vim.keymap.set('n', '<C-k>', vim.lsp.buf.signature_help, bufopts)
-  vim.keymap.set('n', 'gr', vim.lsp.buf.references, bufopts)
+  vim.keymap.set('n', '<leader>r', vim.lsp.buf.references, bufopts)
   vim.keymap.set('n', '<leader>f', function() vim.lsp.buf.format { async = true } end, bufopts)
 end
 
 -- LSP Setup
-local servers = { 'gopls', 'eslint', 'tsserver' }
-for _, lsp in ipairs(servers) do
-  lspconfig[lsp].setup {
-    on_attach = on_attach,
-    capabilities = capabilities,
-  }
-end
+-- local servers = { 'gopls', 'eslint', 'tsserver' }
+-- for _, lsp in ipairs(servers) do
+--   lspconfig[lsp].setup {
+--     on_attach = on_attach,
+--     capabilities = capabilities,
+--   }
+-- end
+
+local servers = {
+  -- clangd = {},
+  gopls = {},
+  -- pyright = {},
+  -- rust_analyzer = {},
+  tsserver = {},
+  eslint = {},
+
+  lua_ls = {
+    Lua = {
+      workspace = { checkThirdParty = false },
+      telemetry = { enable = false },
+    },
+  },
+}
+
+-- mason setup
+-- Setup mason so it can manage external tooling
+require('mason').setup()
+
+-- Ensure the servers above are installed
+local mason_lspconfig = require 'mason-lspconfig'
+
+mason_lspconfig.setup {
+  ensure_installed = vim.tbl_keys(servers),
+}
+
+mason_lspconfig.setup_handlers {
+  function(server_name)
+    require('lspconfig')[server_name].setup {
+      capabilities = capabilities,
+      on_attach = on_attach,
+      settings = servers[server_name],
+    }
+  end,
+}
 
 vim.cmd "autocmd BufWritePre *.tsx,*.ts,*.jsx,*.js EslintFixAll"
 
--- dot_config/nvim/lua/plugins.lua
+++ dot_config/nvim/lua/plugins.lua
@@ -2,32 +2,152 @@ local Plug = vim.fn['plug#']
 local api = vim.api
 local let = vim.g
 
-vim.call('plug#begin', '~/.config/nvim/plugged')
+require('lazy').setup({
+  -- NOTE: First, some plugins that don't require any configuration
 
-Plug('fatih/vim-go', {['do'] = ':GoInstallBinaries', ['for'] = 'go' })
-Plug('hashivim/vim-terraform')
-Plug('hrsh7th/cmp-nvim-lsp')
-Plug('hrsh7th/nvim-cmp')
-Plug('iamcco/markdown-preview.nvim', { ['for'] = {'markdown', 'vim-plug'} })
-Plug('kyazdani42/nvim-web-devicons')
-Plug('L3MON4D3/LuaSnip')
-Plug('mboughaba/i3config.vim')
-Plug('morhetz/gruvbox')
-Plug('nathom/filetype.nvim')
-Plug('neovim/nvim-lspconfig')
-Plug('nvim-lua/plenary.nvim')
-Plug('nvim-lualine/lualine.nvim')
-Plug('nvim-telescope/telescope.nvim', {['tag'] = '0.1.0'})
-Plug('preservim/nerdtree')
-Plug('saadparwaiz1/cmp_luasnip')
-Plug('tpope/vim-commentary')
-Plug('tpope/vim-fugitive')
-Plug('tpope/vim-surround')
-Plug('windwp/nvim-autopairs')
-Plug('Xuyuanp/nerdtree-git-plugin')
-Plug('yuttie/comfortable-motion.vim')
+  -- Git related plugins
+  'tpope/vim-fugitive',
+  'tpope/vim-rhubarb',
 
-vim.call'plug#end'
+  -- surround commands
+  'tpope/vim-surround',
+  'windwp/nvim-autopairs',
+
+  'nathom/filetype.nvim',
+
+  -- Detect tabstop and shiftwidth automatically
+  'tpope/vim-sleuth',
+
+  -- NOTE: This is where your plugins related to LSP can be installed.
+  --  The configuration is done below. Search for lspconfig to find it below.
+  { -- LSP Configuration & Plugins
+    'neovim/nvim-lspconfig',
+    dependencies = {
+      -- Automatically install LSPs to stdpath for neovim
+      'williamboman/mason.nvim',
+      'williamboman/mason-lspconfig.nvim',
+
+      -- Useful status updates for LSP
+      -- NOTE: `opts = {}` is the same as calling `require('fidget').setup({})`
+      { 'j-hui/fidget.nvim', opts = {} },
+
+      -- Additional lua configuration, makes nvim stuff amazing!
+      'folke/neodev.nvim',
+    },
+  },
+
+  { -- Autocompletion
+    'hrsh7th/nvim-cmp',
+    dependencies = { 'hrsh7th/cmp-nvim-lsp', 'L3MON4D3/LuaSnip', 'saadparwaiz1/cmp_luasnip' },
+  },
+
+  -- Useful plugin to show you pending keybinds.
+  { 'folke/which-key.nvim', opts = {} },
+  { -- Adds git releated signs to the gutter, as well as utilities for managing changes
+    'lewis6991/gitsigns.nvim',
+    opts = {
+      -- See `:help gitsigns.txt`
+      signs = {
+        add = { text = '+' },
+        change = { text = '~' },
+        delete = { text = '_' },
+        topdelete = { text = '‾' },
+        changedelete = { text = '~' },
+      },
+    },
+  },
+
+  { 
+    'morhetz/gruvbox',
+    priority = 1000,
+    config = function()
+      vim.cmd.colorscheme 'gruvbox'
+    end,
+  },
+
+  { -- Set lualine as statusline
+    'nvim-lualine/lualine.nvim',
+    -- See `:help lualine.txt`
+    opts = {
+      options = {
+        icons_enabled = false,
+        theme = 'gruvbox',
+        component_separators = '|',
+        section_separators = '',
+      },
+    },
+  },
+
+  { -- Add indentation guides even on blank lines
+    'lukas-reineke/indent-blankline.nvim',
+    -- Enable `lukas-reineke/indent-blankline.nvim`
+    -- See `:help indent_blankline.txt`
+    opts = {
+      char = '┊',
+      show_trailing_blankline_indent = false,
+    },
+  },
+
+  -- "gc" to comment visual regions/lines
+  { 'tpope/vim-commentary' },
+
+  -- Fuzzy Finder (files, lsp, etc)
+  { 'nvim-telescope/telescope.nvim', version = '*', dependencies = { 'nvim-lua/plenary.nvim' } },
+
+  -- Fuzzy Finder Algorithm which requires local dependencies to be built.
+  -- Only load if `make` is available. Make sure you have the system
+  -- requirements installed.
+  {
+    'nvim-telescope/telescope-fzf-native.nvim',
+    -- NOTE: If you are having trouble with this installation,
+    --       refer to the README for telescope-fzf-native for more instructions.
+    build = 'make',
+    cond = function()
+      return vim.fn.executable 'make' == 1
+    end,
+  },
+
+  { -- Highlight, edit, and navigate code
+    'nvim-treesitter/nvim-treesitter',
+    dependencies = {
+      'nvim-treesitter/nvim-treesitter-textobjects',
+    },
+    config = function()
+      pcall(require('nvim-treesitter.install').update { with_sync = true })
+    end,
+  },
+
+  { 'yuttie/comfortable-motion.vim' },
+
+  {
+          'preservim/nerdtree',
+          config = function()
+                  let.NERDTreeMinimalUI = 1
+                  let.NERDTreeDirArrows = 1
+                  let.NERDTreeQuitOnOpen = 1
+                  let.NERDTreeAutoDeleteBuffer = 1
+          end,
+  },
+
+  {'Xuyuanp/nerdtree-git-plugin'},
+
+  -- NOTE: Next Step on Your Neovim Journey: Add/Configure additional "plugins" for kickstart
+  --       These are some example plugins that I've included in the kickstart repository.
+  --       Uncomment any of the lines below to enable them.
+  -- require 'kickstart.plugins.autoformat',
+  -- require 'kickstart.plugins.debug',
+
+  -- NOTE: The import below automatically adds your own plugins, configuration, etc from `lua/custom/plugins/*.lua`
+  --    You can use this folder to prevent any conflicts with this init.lua if you're interested in keeping
+  --    up-to-date with whatever is in the kickstart repo.
+  --
+  --    For additional information see: https://github.com/folke/lazy.nvim#-structuring-your-plugins
+  --
+  --    An additional note is that if you only copied in the `init.lua`, you can just comment this line
+  --    to get rid of the warning telling you that there are not plugins in `lua/custom/plugins/`.
+  -- { import = 'custom.plugins' },
+}, {})
+
 
 -- Comfortable Motion
 vim.g.comfortable_motion_no_default_key_mappings = 1
@@ -35,12 +155,6 @@ vim.g.comfortable_motion_friction=130.0
 vim.g.comfortable_motion_air_drag=2.2
 
 -- NERDTree
-
-let.NERDTreeMinimalUI = 1
-let.NERDTreeDirArrows = 1
-let.NERDTreeQuitOnOpen = 1
-let.NERDTreeAutoDeleteBuffer = 1
-
 -- Open a Nerdtree when no args
 api.nvim_create_autocmd('StdinReadPre', {
     command = 'let s:std_in=1'
@@ -48,6 +162,4 @@ api.nvim_create_autocmd('StdinReadPre', {
 api.nvim_create_autocmd('VimEnter', {
     command = 'if argc() == 0 && !exists("s:std_in") | NERDTree | endif'
 })
-
-require("nvim-autopairs").setup{}
 
