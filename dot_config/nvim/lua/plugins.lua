7f676f89f30b869ab8012130eef4efd36484acf4
nkzren <renan.nakazawa0@gmail.com>
2024-06-18 13:28:24 -0300

    update markdown setup

--- dot_config/nvim/lua/plugins.lua
+++ dot_config/nvim/lua/plugins.lua
@@ -5,7 +5,17 @@ require('lazy').setup({
   require 'plugins/treesitter',
   require 'plugins/completions',
 
-  { 'iamcco/markdown-preview.nvim', ft = 'markdown' },
+  {
+    'MeanderingProgrammer/markdown.nvim',
+    name = 'render-markdown', -- Only needed if you have another plugin named markdown.nvim
+    dependencies = { 'nvim-treesitter/nvim-treesitter' },
+    config = function()
+      require('render-markdown').setup({})
+    end,
+    init = function()
+      vim.keymap.set('n', '<leader>M', ':RenderMarkdownToggle<CR>', { noremap=true, silent=true })
+    end,
+  },
   { 'wuelnerdotexe/vim-astro', ft = 'astro' },
   { 'towolf/vim-helm', ft = 'helm' },
   { 'nkzren/vim-go', ft = 'go' },
